f2373aa450664318185fb6c59a7c5b501d47a20f
deepak121 <dipupoudel1@gmail.com>
2026-05-03 08:26:26 +0545

    updates

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from app.routes import documents_router
 from fastapi import FastAPI
+from fastapi import Depends
+from app.db.connection import get_db_connection 
+
+
+
 
 app = FastAPI()
 
 app.include_router(documents_router)
 
+
 @app.get("/")
 async def root():    return {"message": "Welcome to the Document Retrieval API"}
 
 @app.get("/health")
-async def health_check():    return {"status": "healthy"}
+async def health_check(db = Depends(get_db_connection)):
+    with db.cursor() as cur:
+        cur.execute("SELECT 1")
+        result = cur.fetchone()
+        if result and result[0] == 1:    
+            return {"status": "healthy"}
 
+    return {"status": "unhealthy"}  
--- app/routes/documents.py
+++ app/routes/documents.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 from fastapi import Depends
 from app.db.connection import get_db_connection
 from app.models.documents import Document
@@ -17,7 +18,7 @@ def create_document(request: Document, conn = Depends(get_db_connection)):
     return {"message": "Document created"}
 
 @router.get("/")
-async def get_documents(conn = Depends(get_db_connection)): 
+def get_documents(conn = Depends(get_db_connection)): 
     with conn.cursor() as cur:
         cur.execute("SELECT id, title, content, author, content_type, word_count FROM documents")
         rows = cur.fetchall()
@@ -26,7 +27,7 @@ async def get_documents(conn = Depends(get_db_connection)):
     return documents
 
 @router.get("/{id}")
-async def get_document(id: int, conn = Depends(get_db_connection)):    
+def get_document(id: int, conn = Depends(get_db_connection)):    
     with conn.cursor() as cur:
         cur.execute("SELECT id, title, content, author, content_type, word_count FROM documents WHERE id = %s", (id,))
         row = cur.fetchone()
@@ -34,11 +35,11 @@ async def get_document(id: int, conn = Depends(get_db_connection)):
             document = Document(id=row[0], title=row[1], content=row[2], author=row[3], content_type=row[4], word_count=row[5])    
             return document
         else:
-            return {"message": "Document not found"}
+            raise HTTPException(status_code=404, detail="Document not found")
 
 
 @router.delete("/{id}")
-async def delete_document(id: int, conn = Depends(get_db_connection)):
+def delete_document(id: int, conn = Depends(get_db_connection)):
     with conn.cursor() as cur:
         cur.execute("DELETE FROM documents WHERE id = %s", (id,))
         conn.commit()    
